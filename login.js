1ca5c68772e38b13ddd017c5e42f14eaa3a3240f
Michael-Antonni <72481616+Michael-Antonni@users.noreply.github.com>
2022-02-02 20:29:18 -0500

    Update login.js

--- login.js
+++ login.js
@@ -48,7 +48,7 @@ formLoginAdmin.addEventListener('submit', async e => {
             }else{
                 if(document.password === admin.password){
                     console.log("password correcto");
-                    window.location="../admin/admin.html"; 
+                    window.location="admin/admin.html"; 
                 }else{
                     alert("ERROR: Error en contraseña");
                 }
@@ -96,7 +96,7 @@ formLoginDocente.addEventListener('submit', async e => {
                     console.log("password correcto");
                     
                     localStorage.setItem('docente', document.name);
-                    window.location="../docente/docente.html"; 
+                    window.location="docente/docente.html"; 
                     
                 }else{
                     alert("ERROR: Error en contraseña");
